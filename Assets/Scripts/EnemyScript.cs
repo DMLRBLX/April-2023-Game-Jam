699b5b2aa35304a52c900dded9ab3dc5d5cb472b
DMLRBLX <109883821+DMLRBLX@users.noreply.github.com>
2023-04-27 15:15:59 -0600

    Enemy stuff

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -8,26 +8,73 @@ public class EnemyScript : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] float walkPointCushion;
     [SerializeField] float moveSpeed = 15f;
+    [SerializeField] Vector2 maxMinWaitTimes;
 
     Transform walkPoint;
 
     float distance;
 
+    int walkPointCount;
+
+    bool attacking = false;
+    bool thinking = false;
+
+    private void Start()
+    {
+        int randomPoint = Random.Range(0, enemyController.WalkPoints.Length);
+        walkPoint = enemyController.WalkPoints[randomPoint];
+        distance = Vector2.Distance(gameObject.transform.position, walkPoint.position);
+    }
+
     private void Update()
     {
-        if (walkPoint == null | distance <= walkPointCushion)
+        if (!thinking)
+          StartCoroutine(Attack());
+
+        if (!attacking)
         {
-            int randomPoint = Random.Range(0, enemyController.WalkPoints.Length);
-            walkPoint = enemyController.WalkPoints[randomPoint];
             distance = Vector2.Distance(gameObject.transform.position, walkPoint.position);
+
+            if (distance < walkPointCushion)
+            {
+                int randomPoint = Random.Range(0, enemyController.WalkPoints.Length);
+                walkPoint = enemyController.WalkPoints[randomPoint];
+            }
+
+            gameObject.transform.position = Vector2.MoveTowards(this.transform.position, walkPoint.transform.position, (moveSpeed / 10) * Time.deltaTime);
         }
+        else
+        {
+            StartCoroutine(StopAttacking());
+            distance = Vector2.Distance(gameObject.transform.position, player.transform.position);
 
-        Vector2 dir = player.transform.position - gameObject.transform.position;
+            if (distance < walkPointCushion)
+            {
+                int randomPoint = Random.Range(0, enemyController.WalkPoints.Length);
+                walkPoint = enemyController.WalkPoints[randomPoint];
+            }
 
-        dir.Normalize();
-        float ang = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            gameObject.transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, (moveSpeed / 10) * Time.deltaTime);
+        }
+    }
 
-        gameObject.transform.rotation = Quaternion.Euler(Vector3.forward * ang);
-        gameObject.transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, (moveSpeed / 10) * Time.deltaTime);
+    IEnumerator Attack()
+    {
+        thinking = true;
+        float randomTime = Random.Range(maxMinWaitTimes.y, maxMinWaitTimes.x);
+
+        yield return new WaitForSeconds(randomTime);
+
+        attacking = true;
+    }
+
+    IEnumerator StopAttacking()
+    {
+        thinking = true;
+        float randomTime = Random.Range(maxMinWaitTimes.y / 2, maxMinWaitTimes.x / 2);
+
+        yield return new WaitForSeconds(randomTime);
+
+        attacking = false;
     }
 }
